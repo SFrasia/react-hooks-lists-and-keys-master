42c33123b9c62580e6ed7ed85b199faeec85671a
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-02-29 09:49:38 +0300

    Changes

---
+++ src/components/ColorList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+
+  const colorElements = colors.map((color) => {
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Top 5 CSS Colors</h1>
+      <ol>{colorElements}</ol>
+    </div>
+  );
+}
+
+export default ColorList;
